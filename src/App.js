2d9f8be068deca4096eff5bb8383560eea0c3c54
unaldutlu <unaldutlu@gmail.com>
2022-11-09 15:36:23 +0300

    add bootstrap on page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import MenuRouter from "./router/MenuRouter";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className='text-center'>
+      <MenuRouter />
     </div>
   );
 }
